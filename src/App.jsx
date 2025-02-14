b50b262624ca003777d79205c16b54ae572a6858
Chris Fagan <cfagan88@hotmail.co.uk>
2025-02-14 11:46:25 +0000

    adding useReducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,49 @@
 import "./index.css";
-import { useState } from "react";
+import { useState, useReducer } from "react";
 
 /* Functionality to add:
   Account for negative numbers
   Prevent adding a second decimal place, but wont work for adding two numbers both with decimals. e.g. 1.1 + 2.2
   */
 
+function reducer(state, action) {
+  switch (action.type) {
+    case "":
+      return {};
+    case "":
+      return {};
+    case "":
+      return {};
+    case "":
+      return {};
+    case "":
+      return {};
+  }
+}
+
 function App() {
   const digits = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-  const operators = ["+", "-", "*", "/", "."];
-  const [calc, setCalc] = useState("");
-  const [result, setResult] = useState("");
+  const [{ currentOp, prevOp, operation }, dispatch] = useReducer(reducer, {
+    currentOp: 0,
+    prevOp: 0,
+    operation: null,
+  });
 
-  function updateCalc(value) {
-
-  }
-
-  function equals() {
-    
-  }
-
-  function deleteLast() {
-
-    
-  }
-
-  function deleteAll() {
-
-  }
+  function add() {}
+  function subtract() {}
 
   return (
     <main className="bg-gradient-to-br from-cyan-700 to-slate-800 flex items-center justify-center h-screen">
       <div className="rounded-2xl w-[30vw] min-w-[280px]">
         <div className="bg-[#151515] rounded-t-2xl text-gray-200 pr-5 py-4 text-right text-5xl">
-          {result ? (
-            <span className="text-gray-500 text-xl">({result})</span>
-          ) : (
-            ""
-          )}{" "}
-          {calc || 0}
+          <span className="text-gray-500 text-xl">{currentOp}</span>
+          {prevOp || 0}
         </div>
 
         <div className="grid grid-cols-6">
           <button
             onClick={() => {
-              updateCalc("/");
+              handleOperation("/");
             }}
             className="bg-[#692100] h-18 text-gray-200 text-2xl font-bold border-1 border-border-solid border-black cursor-pointer hover:bg-[#772e0c]"
           >
@@ -52,7 +51,7 @@ function App() {
           </button>
           <button
             onClick={() => {
-              updateCalc("*");
+              handleOperation("*");
             }}
             className="bg-[#692100] h-18 text-gray-200 text-2xl font-bold border-1 border-border-solid border-black cursor-pointer hover:bg-[#772e0c]"
           >
@@ -60,7 +59,7 @@ function App() {
           </button>
           <button
             onClick={() => {
-              updateCalc("-");
+              handleOperation("-");
             }}
             className="bg-[#692100] h-18 text-gray-200 text-2xl font-bold border-1 border-border-solid border-black cursor-pointer hover:bg-[#772e0c]"
           >
@@ -68,7 +67,7 @@ function App() {
           </button>
           <button
             onClick={() => {
-              updateCalc("+");
+              handleOperation("+");
             }}
             className="bg-[#692100] h-18 text-gray-200 text-2xl font-bold border-1 border-border-solid border-black cursor-pointer hover:bg-[#772e0c]"
           >
@@ -97,7 +96,7 @@ function App() {
             <button
               key={`${digit}`}
               onClick={() => {
-                updateCalc(digit);
+                handleOperation(digit);
               }}
               className="bg-[#151515] h-18 text-gray-200 text-2xl font-bold border-1 border-border-solid border-black cursor-pointer hover:bg-[#252525]"
             >
@@ -106,7 +105,7 @@ function App() {
           ))}
           <button
             onClick={() => {
-              updateCalc("0");
+              handleOperation("0");
             }}
             className="bg-[#151515] h-18 rounded-bl-2xl text-gray-200 text-2xl font-bold border-1 border-border-solid border-black cursor-pointer hover:bg-[#252525]"
           >
@@ -114,7 +113,7 @@ function App() {
           </button>
           <button
             onClick={() => {
-              updateCalc(".");
+              handleOperation(".");
             }}
             className="bg-[#151515] h-18 text-gray-200 text-2xl font-bold border-1 border-border-solid border-black cursor-pointer hover:bg-[#252525]"
           >
