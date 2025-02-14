05f7161f8779429869c300968fc6205c8bf129bf
Chris Fagan <cfagan88@hotmail.co.uk>
2025-02-14 13:41:51 +0000

    set up reducer function

--- src/App.jsx
+++ src/App.jsx
@@ -1,90 +1,83 @@
 import "./index.css";
-import { useState, useReducer } from "react";
+import { useReducer } from "react";
 
 /* Functionality to add:
   Account for negative numbers
   Prevent adding a second decimal place, but wont work for adding two numbers both with decimals. e.g. 1.1 + 2.2
   */
 
-function reducer(state, action) {
-  switch (action.type) {
-    case "":
+// https://www.youtube.com/watch?v=DgRrrOt0Vr8&t=550s
+
+const ACTIONS = {
+  ADD_DIGIT: "add-digit",
+  CHOOSE_OPERATION: "choose-operation",
+  CLEAR: "clear",
+  DELETE_DIGIT: "delete-digit",
+  EQUALS: "equals",
+};
+
+function reducer(state, { type, payload }) {
+  switch (type) {
+    case ACTIONS.ADD_DIGIT:
+      return { ...state, currentOp: `${currentOp || ""}${payload.digit}` };
+    case ACTIONS.CHOOSE_OPERATION:
       return {};
-    case "":
+    case ACTIONS.CLEAR:
       return {};
-    case "":
+    case ACTIONS.DELETE_DIGIT:
       return {};
-    case "":
-      return {};
-    case "":
+    case ACTIONS.EQUALS:
       return {};
   }
 }
 
 function App() {
   const digits = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-  const [{ currentOp, prevOp, operation }, dispatch] = useReducer(reducer, {
-    currentOp: 0,
-    prevOp: 0,
-    operation: null,
-  });
-
-  function add() {}
-  function subtract() {}
+  const [{ currentOp, prevOp, operation }, dispatch] = useReducer(reducer, {});
 
   return (
     <main className="bg-gradient-to-br from-cyan-700 to-slate-800 flex items-center justify-center h-screen">
       <div className="rounded-2xl w-[30vw] min-w-[280px]">
         <div className="bg-[#151515] rounded-t-2xl text-gray-200 pr-5 py-4 text-right text-5xl">
-          <span className="text-gray-500 text-xl">{currentOp}</span>
-          {prevOp || 0}
+          <div>
+            {prevOp} {operation}
+          </div>
+          <div>{currentOp}</div>
         </div>
 
         <div className="grid grid-cols-6">
           <button
-            onClick={() => {
-              handleOperation("/");
-            }}
+            onClick={() => {}}
             className="bg-[#692100] h-18 text-gray-200 text-2xl font-bold border-1 border-border-solid border-black cursor-pointer hover:bg-[#772e0c]"
           >
             /
           </button>
           <button
-            onClick={() => {
-              handleOperation("*");
-            }}
+            onClick={() => {}}
             className="bg-[#692100] h-18 text-gray-200 text-2xl font-bold border-1 border-border-solid border-black cursor-pointer hover:bg-[#772e0c]"
           >
             x
           </button>
           <button
-            onClick={() => {
-              handleOperation("-");
-            }}
+            onClick={() => {}}
             className="bg-[#692100] h-18 text-gray-200 text-2xl font-bold border-1 border-border-solid border-black cursor-pointer hover:bg-[#772e0c]"
           >
             -
           </button>
           <button
-            onClick={() => {
-              handleOperation("+");
-            }}
+            onClick={() => {}}
             className="bg-[#692100] h-18 text-gray-200 text-2xl font-bold border-1 border-border-solid border-black cursor-pointer hover:bg-[#772e0c]"
           >
             +
           </button>
           <button
-            onClick={() => {
-              deleteLast();
-            }}
+            onClick={() => {}}
             className="bg-[#692100] h-18 text-gray-200 text-2xl font-bold border-1 border-border-solid border-black cursor-pointer hover:bg-[#772e0c]"
           >
             DEL
           </button>
           <button
-            onClick={() => {
-              deleteAll();
-            }}
+            onClick={() => {}}
             className="bg-[#692100] h-18 text-gray-200 text-2xl font-bold border-1 border-border-solid border-black cursor-pointer hover:bg-[#772e0c]"
           >
             CE
@@ -96,33 +89,27 @@ function App() {
             <button
               key={`${digit}`}
               onClick={() => {
-                handleOperation(digit);
+                dispatch({ type: ACTIONS.ADD_DIGIT, payload: { digit } });
               }}
               className="bg-[#151515] h-18 text-gray-200 text-2xl font-bold border-1 border-border-solid border-black cursor-pointer hover:bg-[#252525]"
             >
-              {digit.toString()}
+              {digit}
             </button>
           ))}
           <button
-            onClick={() => {
-              handleOperation("0");
-            }}
+            onClick={() => {}}
             className="bg-[#151515] h-18 rounded-bl-2xl text-gray-200 text-2xl font-bold border-1 border-border-solid border-black cursor-pointer hover:bg-[#252525]"
           >
             0
           </button>
           <button
-            onClick={() => {
-              handleOperation(".");
-            }}
+            onClick={() => {}}
             className="bg-[#151515] h-18 text-gray-200 text-2xl font-bold border-1 border-border-solid border-black cursor-pointer hover:bg-[#252525]"
           >
             .
           </button>
           <button
-            onClick={() => {
-              equals();
-            }}
+            onClick={() => {}}
             className="bg-[#151515] h-18 rounded-br-2xl text-gray-200 text-xl font-bold border-1 border-border-solid border-black cursor-pointer hover:bg-[#252525]"
           >
             =
